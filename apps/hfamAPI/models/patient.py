9030f8297c6428c650d31d48246b5511aeec1ba9
htouloupas <harrytouloupas@gmail.com>
2020-04-04 21:13:21 +0300

    charfield needed max length and applied migrations

--- apps/hfamAPI/models/patient.py
+++ apps/hfamAPI/models/patient.py
@@ -11,5 +11,5 @@ class Patient(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     state = models.CharField(max_length=15, choices=patientStates, default="1")
     disease = models.OneToOneField(Disease, on_delete=models.SET_NULL, null=True, related_name="disease_of")
-    previousDiseases = models.CharField(null=True, choices=previousDiseases)
+    previousDiseases = models.CharField(max_length=15, null=True, choices=previousDiseases)
     previousMedicine = models.TextField(null=True)
