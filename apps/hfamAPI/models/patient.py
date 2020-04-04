bb5962da1874e7db09875d186e3858593e1ab0ec
htouloupas <harrytouloupas@gmail.com>
2020-04-04 21:10:14 +0300

    added patients fields and fixed disease

--- apps/hfamAPI/models/patient.py
+++ apps/hfamAPI/models/patient.py
@@ -4,9 +4,12 @@ from .disease import Disease
 from .hospital import Hospital
 
 patientStates = [("1", "Diagnosed"), ("2", "Hospitalized"), ("3", "Discharged")]
+previousDiseases = [("1", "Respiratory"), ("2", "Heart"), ("3", "Inflammatory")]
 
 
 class Patient(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     state = models.CharField(max_length=15, choices=patientStates, default="1")
     disease = models.OneToOneField(Disease, on_delete=models.SET_NULL, null=True, related_name="disease_of")
+    previousDiseases = models.CharField(null=True, choices=previousDiseases)
+    previousMedicine = models.TextField(null=True)
