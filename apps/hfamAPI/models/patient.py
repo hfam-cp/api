241d3ecb9d53f57127933d5fce57f88f0efceffd
htouloupas <harrytouloupas@gmail.com>
2020-04-04 21:23:04 +0300

    fixed patient states and previous Diseases

--- apps/hfamAPI/models/patient.py
+++ apps/hfamAPI/models/patient.py
@@ -3,13 +3,13 @@ from django.db import models
 from .disease import Disease
 from .hospital import Hospital
 
-patientStates = [("1", "Diagnosed"), ("2", "Hospitalized"), ("3", "Discharged")]
-previousDiseases = [("1", "Respiratory"), ("2", "Heart"), ("3", "Inflammatory")]
+patientStates = [("DIAGNOSED", "Diagnosed"), ("HOSPITALIZED", "Hospitalized"), ("DISCHARGED", "Discharged")]
+previousDiseases = [("RESPITATORY", "Respiratory"), ("HEART", "Heart"), ("INFLAMMATORY", "Inflammatory")]
 
 
 class Patient(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    state = models.CharField(max_length=15, choices=patientStates, default="1")
+    state = models.CharField(max_length=15, choices=patientStates, default="DIAGNOSED")
     disease = models.OneToOneField(Disease, on_delete=models.SET_NULL, null=True, related_name="disease_of")
     previousDiseases = models.CharField(max_length=15, null=True, choices=previousDiseases)
     previousMedicine = models.TextField(null=True)
