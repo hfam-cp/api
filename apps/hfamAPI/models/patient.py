e997ac705959a38c7d9927650e1337c59b5283ad
htouloupas <harrytouloupas@gmail.com>
2020-04-04 23:13:47 +0300

    patients fix

--- apps/hfamAPI/models/patient.py
+++ apps/hfamAPI/models/patient.py
@@ -10,6 +10,6 @@ previousDiseases = [("RESPITATORY", "Respiratory"), ("HEART", "Heart"), ("INFLAM
 class Patient(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     state = models.CharField(max_length=15, choices=patientStates, default="DIAGNOSED")
-    disease = models.OneToOneField(Disease, on_delete=models.SET_NULL, null=True, related_name="disease_of")
+    disease = models.ForeignKey(Disease, on_delete=models.SET_NULL, null=True, related_name="disease_of")
     previousDiseases = models.CharField(max_length=15, null=True, choices=previousDiseases)
     previousMedicine = models.TextField(null=True)
